f3caeadba6a55ebcd02d3882a2ad47265971c743
M-Wasilewski92 <Mwasilewski92@gmail.com>
2025-03-05 00:01:43 +0100

    Add base path support for generated links

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_page, generate_pages_recursive
@@ -8,13 +9,15 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 project_directory = os.path.dirname(base_dir)
 
 dir_path_static = os.path.join(project_directory, "static")
-dir_path_public = os.path.join(project_directory, "public")
+dir_path_public = os.path.join(project_directory, "docs")
 dir_path_content = os.path.join(project_directory, "content")
 template_path = os.path.join(project_directory, "template.html")
 
 
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -23,7 +26,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 if __name__ == '__main__':
